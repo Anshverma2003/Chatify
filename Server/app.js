85e71fe14b4a956d7aaa01b5f7b232706e1a100f
Ansh Verma <vermaansh708@gmail.com>
2024-03-30 18:25:16 +0530

    group chat

--- Server/app.js
+++ Server/app.js
@@ -9,22 +9,28 @@ const port = 8080;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        methods: ["GET" , "POST"]
+        origin: ['http://localhost:3000', 'chrome-extension://pianhoblplginndplnpobnghlmooiicc'],
+        methods: ["GET", "POST"]
     }
 });
 
 io.on('connection', (socket) => {
-    console.log('user connnected' , socket.id);
-    socket.on('disconnect' , ()=>{
-        console.log('user disConnected' , socket.id);
-    })
+    console.log('user connected', socket.id);
 
-    socket.on('message' ,({id , data})=>{
+    socket.on('message', ({ id, data }) => {
         console.log(data);
-        io.to(id).emit('receivedMessage' , data);
-    })
-})
+        socket.to(id).emit('receivedMessage', data);
+    });
+
+    socket.on('join-room', (room) => {
+        socket.join(room);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+    });
+});
+
 
 server.listen(port, () => {
     console.log(`Server is running at port ${port}`);
--- client/src/Component/home.jsx
+++ client/src/Component/home.jsx
@@ -1,25 +1,33 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import './home.css'
 import { io } from 'socket.io-client';
 
 const Home = () => {
     const [id, setId] = useState('');
     const [data, setData] = useState('');
-    const [socketId , setSocketId] = useState('');
-    const [response , setRespone] = useState([]);
-    const socket = useMemo(()=> io('http://localhost:8080'),[]);
+    const [socketId, setSocketId] = useState('');
+    const [response, setRespone] = useState([]);
+    const [room, setRoom] = useState('');
+    const [roomVisible, setRoomVisible] = useState(false);
+    const [singleChat, setSingleChat] = useState(true);
+
+    const socket = useMemo(() => io('http://localhost:8080'), []);
 
 
     useEffect(() => {
-
-        socket.on('receivedMessage' , (message)=>{
-            // console.log(message);
-            setRespone(message);
-        })
+        socket.on('receivedMessage', (message) => {
+            setRespone(prevResponse => [...prevResponse, message]);
+        });
 
         socket.on('connect', () => {
             console.log('connected', socket.id);
             setSocketId(socket.id);
         });
+
+        return () => {
+            socket.off('receivedMessage');
+            socket.off('connect');
+        };
     }, []);
 
     const handleSubmit = (e) => {
@@ -30,25 +38,58 @@ const Home = () => {
         console.log('Form submitted with id:', id, 'and data:', data);
     };
 
+    const handleJoinRoom = (e) => {
+        e.preventDefault();
+        socket.emit('join-room', room);
+    }
+    const toggleRoomVisibility = () => {
+        setRoomVisible(prevState => !prevState);
+        setSingleChat(prevState => !prevState)
+        setRespone('');
+    };
+
+
     return (
         <div className="home">
-            <h2>Your ID: {socketId} </h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="id">ID:</label>
-                    <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
-                </div>
-                <div>
-                    <label htmlFor="data">Data:</label>
-                    <input type="text" id="data" value={data} onChange={(e) => setData(e.target.value)} required />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
 
-            {response && <div className="response">
-                <p>{response}</p>
+            <button onClick={toggleRoomVisibility}>{roomVisible ? "Single Chat" : "Group Chat"}</button>
+            {singleChat && <div>
+                <h2>Your ID: {socketId} </h2>
+                <form className="chat" onSubmit={handleSubmit}>
+                    <label className="to-id" >To: <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+
+                    </label>
+                    <label htmlFor="data" className="input-field">Message:
+                        <textarea type="text" id="data" value={data} onChange={(e) => setData(e.target.value)} required />
+                    </label>
+                    <button className="submit-single-chat" type="submit">Submit</button>
+
+                </form>
             </div>}
+
+            {roomVisible && <div className="room">
+                <div className='group-chat'>
+                    <form className='room-chat' onSubmit={handleJoinRoom}>
+                        <label className="to-id">Join Room:
+                            <input type="text" onChange={(e) => { setRoom(e.target.value); setId(e.target.value) }} />
+                            <button type="submit" className='join'>Join</button>
+                        </label>
+                    </form>
+                    <form className='chat' onSubmit={handleSubmit}>
+                        <label htmlFor="data" className="input-field">Message:
+                            <textarea type="text" id="data" value={data} onChange={(e) => setData(e.target.value)} required />
+                        </label>
+                        <button type="submit" className='submit-group-chat'>Submit</button>
+                    </form>
+                </div>
+            </div>}
+            <div className="messages-container">
+                {response && response.map((msg, index) => (
+                    <p key={index}>{msg}</p>
+                ))}
+            </div>
         </div>
+
     );
 };
 
