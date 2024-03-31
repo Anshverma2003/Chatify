e91572eff5c4a45083ea82d5cf6cf527196ee6f6
Ansh Verma <vermaansh708@gmail.com>
2024-03-31 12:55:14 +0530

    cors fixed

--- client/src/Component/home.jsx
+++ client/src/Component/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     const [roomVisible, setRoomVisible] = useState(false);
     const [singleChat, setSingleChat] = useState(true);
 
-    const socket = useMemo(() => io('https://chatify-5pzqcpm73-ansh-vermas-projects-a57df2a8.vercel.app'), []);
+    const socket = useMemo(() => io('https://chatify-nine-coral.vercel.app/'), []);
 
 
 
